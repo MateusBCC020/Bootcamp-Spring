680e20bd01fe581c6ebb0bc40626470e10fe2f9c
MateusBCC020 <mateusdossantosribeiro100@usp.br>
2020-11-04 08:14:26 -0300

    Spring - Exercício Back end, code blog

--- blogPessoal/src/main/java/org/mateusribeiro/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/mateusribeiro/blogPessoal/model/Postagem.java
@@ -8,6 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.sun.istack.NotNull;
+
 @Entity
 @Table(name = "postagem")
 public class Postagem {
@@ -16,6 +18,7 @@ public class Postagem {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
+	@NotNull
 	private String titulo;
 	
 	private String texto;
