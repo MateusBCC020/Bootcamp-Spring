9206127e26153e3df486a8845307ada2451e9076
MateusBCC020 <mateusdossantosribeiro100@usp.br>
2020-11-09 15:53:11 -0300

    Criação do relacionamento de entidades do projeto de aulas online

--- Learn/src/main/java/org/vestibulandosdacidadania/Learn/model/Aula.java
+++ Learn/src/main/java/org/vestibulandosdacidadania/Learn/model/Aula.java
@@ -6,41 +6,44 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "tb_aula")
 public class Aula {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@NotNull
 	@Size(min = 5, max = 255)
 	private String titulo;
-	
+
+	@NotNull
+	@Size(min = 5, max = 255)
+	private String tema;
+
 	@NotNull
 	private String autor;
-	
+
 	@NotNull
 	@Size(min = 5, max = 4096)
 	private String texto;
-	
-	public String getTexto() {
-		return texto;
-	}
-
-	public void setTexto(String texto) {
-		this.texto = texto;
-	}
 
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date data = new java.sql.Date(System.currentTimeMillis());
+
+	@ManyToOne
+	@JsonIgnoreProperties("aula")
+	private Materia materia;
 
 	public long getId() {
 		return id;
@@ -58,12 +61,28 @@ public class Aula {
 		this.titulo = titulo;
 	}
 
+	public String getTema() {
+		return tema;
+	}
+
+	public void setTema(String tema) {
+		this.tema = tema;
+	}
+
 	public String getAutor() {
 		return autor;
 	}
 
 	public void setAutor(String autor) {
 		this.autor = autor;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
 	}
 
 	public Date getData() {
@@ -73,5 +92,13 @@ public class Aula {
 	public void setData(Date data) {
 		this.data = data;
 	}
-	
+
+	public Materia getMateria() {
+		return materia;
+	}
+
+	public void setMateria(Materia materia) {
+		this.materia = materia;
+	}
+
 }
