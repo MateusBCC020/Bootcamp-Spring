f70526cf753278e427f123d4c89d237de6ff95c2
MateusBCC020 <mateusdossantosribeiro100@usp.br>
2020-11-14 20:02:00 -0300

    Configuração da camada de segurança da aplicação

--- blogPessoal/src/main/java/org/mateus/blogPessoal/model/Usuario.java
+++ blogPessoal/src/main/java/org/mateus/blogPessoal/model/Usuario.java
@@ -22,7 +22,7 @@ public class Usuario {
 	
 	@NotNull
 	@Size(min = 8, max = 255)
-	private String email;
+	private String usuario;
 	
 	@NotNull
 	@Size(min = 8, max = 16)
@@ -44,12 +44,12 @@ public class Usuario {
 		this.nome = nome;
 	}
 
-	public String getEmail() {
-		return email;
+	public String getUsuario() {
+		return usuario;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
 	}
 
 	public String getSenha() {
